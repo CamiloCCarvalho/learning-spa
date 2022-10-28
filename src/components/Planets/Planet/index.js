0dbe490407186320563c845fb7c6cae560a32061
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-10-28 03:56:09 -0300

    Commit Att
    Resolvido problema com renderizaçãode multiplos components usando Array.map() em vez de indexar usando o ID do array, criado objetos no array com a propriedade ID que é passado como Key pros Elementos a serem gerados pelo Loop

--- src/components/Planets/Planet/index.js
+++ src/components/Planets/Planet/index.js
@@ -3,6 +3,15 @@ import {GrayImg} from '../../shared/gray_img'
 import {DescriptionWithLink} from '../../shared/descriptionWithLink'
 
 export const Planet = (props) => {
+    const listNames = [
+        {id: 0, name: 'a'},
+        {id: 1, name: 'b'},
+        {id: 2, name: 'c'}
+    ]
+    const satellites = listNames.map(item =>
+         <li key={item.id}>Satelite "{item.name}"</li>
+    )
+
     let title;
     if(props.title_with_underline)
         title = <h4><u>{props.name}</u></h4>
@@ -17,6 +26,12 @@ export const Planet = (props) => {
             <DescriptionWithLink descriptionText={props.descriptionText} linkHREF={props.linkHREF}></DescriptionWithLink>
             <hr/>
             <GrayImg img_url={props.img_url} gray={props.gray}/>
+
+            <h4>Lista de Satelites:</h4>
+            <ul>
+                {satellites}
+            </ul>
+            <hr/>
         </div>
     )
 }
