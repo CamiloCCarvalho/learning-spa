0b955e0c4052cd2bdaeeae2fd6b0ba986c443bf5
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-11-07 11:55:04 -0300

    Commit Att

---
+++ src/components/aboutHome/index.js
@@ -0,0 +1,7 @@
+import {React, Fragment} from 'react'
+
+
+
+export const AboutHome = () => {
+    const [stateNow, setState] = useState({})
+}
--- src/components/sessionHome/index.js
+++ src/components/sessionHome/index.js
@@ -3,21 +3,22 @@ import './sessionHome.css'
 
 
 export const SessionHome = ()  => {
-    const flipCardStyle = {
-        width: "300px",
-        height: "300px"
-    }
     return (
         <Fragment>
             <div className="sessionHome">
                 <p className="display-2" id="home-hi">Hello!</p>
                 <p className="display-1" id="home-name">Eu sou <i>Camilo</i></p>
-                <p className="display-5 mt-3" id="home-content">Front-End Developer Jr. Criando aplicações WEB com React, BootStrap, JS e outras Stacks.</p>
+                <p className="display-5 mt-3" id="home-content">Front-End Developer Jr. Desenvolvimento web com React, BootStrap, JS e outras Stacks.</p>
 
-                <div className="flip-card">
+                <div className="flip-card mt-5">
                     <div className="flip-card-inner">
                         <div className="flip-card-front">
                             <div id="imageDev"></div>
+                        </div>
+                        <div className="flip-card-back">
+                            <h1>Camilo Costa</h1>
+                            <p>FRONT-END Jr.</p>
+                            <p>Estudando React e APIs</p>
                         </div>
                     </div>
                 </div>
