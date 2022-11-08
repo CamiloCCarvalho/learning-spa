f31f8b5eaa6e1c971c42b4e3574074773e10f284
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-11-07 22:26:57 -0300

    Commit Att

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import {NavBar} from './components/navbar'
 import {SessionHome} from './components/sessionHome'
 import {AboutHome} from './components/aboutHome'
+import {PortHome} from './components/portHome'
 
 function App() {
   return (
--- src/components/aboutHome/container/stacks/index.js
+++ src/components/aboutHome/container/stacks/index.js
@@ -1,100 +1,41 @@
-import {React, Fragment, useState, useEffect} from 'react'
-
-async function getStacks(){
-    let response = await fetch('%PUBLIC_URL%/API/stacks.json')
-    let data = await response.json()
-    return data
-}
+import {React, Fragment} from 'react'
+import './stacks.css'
 
 export const Stacks = () => {
-    const [stack, setStack] = useState([])
-
-    useEffect(() => {
-        getStacks().then( data => {
-            setStack(data["stacks"])
-        }, error => {
-            console.log(`Algo deu errado com a requisição JSON - ${error}`)
-        })
-    }, [])
 
     return (
         <Fragment>
-        <div className="col-md coll">
-            <div className="row g-3">
+            <div className="col-md coll">
+            <div className="row text-center">
+
+
                 <div className="col-lg coll">
-
-                    {stack.map(item => {
-                        return (
-                        <div className="col-log coll">
-                            <a href={item.link}>
-                                <img className="logo" alt={item.name} src={item.image_ul}/>
-                            </a>
-                        </div>
-                        ) 
-                    })}
-
                     <a href="https://reactjs.org/">
-                        <img className="logo" alt="React" src="https://user-images.githubusercontent.com/106208340/199318462-58be3b04-8c48-488f-b8c3-384afc04bfdc.png"/> 
+                        <img className="logo" alt="React" src="https://user-images.githubusercontent.com/106208340/199318462-58be3b04-8c48-488f-b8c3-384afc04bfdc.png"/>
                     </a>
-
-
-
                 </div>
                 <div className="col-lg coll">
                     <a href="https://www.javascript.com/">
-                        <img className="logo" alt="JavaScript" widht="48px" heigth="48px" src="https://img.icons8.com/color/48/000000/javascript--v1.png"/> 
+                        <img className="logo" alt="JavaScript" src="https://img.icons8.com/color/48/000000/javascript--v1.png"/>
                     </a>
                 </div>
                 <div className="col-lg coll">
                     <a href="https://www.w3schools.com/html/">
-                        <img className="logo" alt="Html" src="https://img.icons8.com/fluency/48/000000/html-5.png"/> 
+                        <img className="logo" alt="Html" src="https://img.icons8.com/fluency/48/000000/html-5.png"/>
                     </a>
                 </div>
+
             </div>
+            <div className="row text-center">
 
-
-            <div className="row g-3">
                 <div className="col-lg coll">
                     <a href="https://www.w3schools.com/css/">
-                        <img className="logo" alt="Css" widht="48px" heigth="48px" src="https://img.icons8.com/fluency/48/000000/css3.png"/> 
+                        <img className="logo" alt="Css" src="https://img.icons8.com/fluency/48/000000/css3.png"/>
                     </a>
                 </div>
                 <div className="col-lg coll">
                     <a href="https://sass-lang.com/">
-                        <img className="logo" alt="Sass" widht="48px" heigth="48px" src="https://img.icons8.com/color/48/000000/sass.png"/> 
-                    </a>
-                </div>
-                <div className="col-lg coll">
-                    <a href="https://getbootstrap.com/">
-                        <img className="logo" alt="BootStrap" widht="48px" heigth="48px" src="https://img.icons8.com/color/48/000000/bootstrap.png"/> 
-                    </a>
-                </div>
-            </div>
-
-
-            <div className="row g-3">
-                <div className="col-lg coll">
-                    <a href="https://www.npmjs.com/">
-                        <img className="logo" alt="Npm" widht="48px" heigth="48px" src="https://img.icons8.com/color/48/000000/npm.png"/>
-                    </a>
-                </div>
-                <div className="col-lg coll">
-                    <a href="https://nodejs.org/pt-br/">
-                        <img className="logo" alt="Node" widht="48px" heigth="48px" src="https://img.icons8.com/fluency/48/000000/node-js.png"/> 
-                    </a>
-                </div>
-                <div className="col-lg coll">
-                    <a href="https://babeljs.io/">
-                        <img className="logo" alt="Babel" width="56" height="56" src="https://d33wubrfki0l68.cloudfront.net/7a197cfe44548cc1a3f581152af70a3051e11671/78df8/img/babel.svg"/>  
-                    </a>
-                </div>
-            </div>
-
-
-            <div className="row g-3">
-                <div className="col-lg coll">
-                    <a href="https://webpack.js.org/">
-                        <img className="logo" alt="WebPack" src="https://img.icons8.com/color/48/000000/webpack.png"/> 
+                        <img className="logo" alt="Sass" src="https://img.icons8.com/color/48/000000/sass.png"/>
                     </a>
                 </div>
                 <div className="col-lg coll">
@@ -102,15 +43,47 @@ export const Stacks = () => {
                         <img className="logo" alt="Git" src="https://img.icons8.com/color/48/000000/git.png"/>
                     </a>
                 </div>
+                
+            </div>
+            <div className="row text-center">
+
                 <div className="col-lg coll">
-                    <a href="https://github.com/">
-                        <img className="logo" alt="GitHub" src="https://img.icons8.com/color/48/000000/github--v1.png"/> 
+                    <a href="https://getbootstrap.com/">
+                        <img className="logo" alt="Bootstrap" src="https://img.icons8.com/color/48/000000/bootstrap.png"/>
                     </a>
                 </div>
+                <div className="col-lg coll">
+                    <a href="https://www.npmjs.com/">
+                        <img className="logo" alt="Npm" src="https://img.icons8.com/color/48/000000/npm.png"/>
+                    </a>
+                </div>
+                <div className="col-lg coll">
+                    <a href="https://nodejs.org/pt-br/">
+                        <img className="logo" alt="Node" src="https://img.icons8.com/fluency/48/000000/node-js.png"/>
+                    </a>
+                </div>
+                
             </div>
+            <div className="row text-center">
 
+                <div className="col-lg coll">
+                    <a href="https://github.com/">
+                        <img className="logo" alt="GitHub" src="https://img.icons8.com/color/48/000000/github--v1.png"/>
+                    </a>
+                </div>
+                <div className="col-lg coll">
+                    <a href="https://babeljs.io/">
+                        <img className="logo"  width="86px" alt="Babel" src="https://d33wubrfki0l68.cloudfront.net/7a197cfe44548cc1a3f581152af70a3051e11671/78df8/img/babel.svg"/>
+                    </a>
+                </div>
+                <div className="col-lg coll">
+                    <a href="https://webpack.js.org/">
+                        <img className="logo" alt="Webpack" src="https://img.icons8.com/color/48/000000/webpack.png"/>
+                    </a>
+                </div>
+                
+            </div>
         </div>
-        </Fragment>
-        
+        </Fragment> 
     )
 }
