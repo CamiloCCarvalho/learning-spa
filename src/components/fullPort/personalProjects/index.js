e455537d96aa0b4620fef75c5188c2e03b21a55b
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2022-11-13 18:46:34 -0300

    Commit Att

--- src/components/fullPort/personalProjects/index.js
+++ src/components/fullPort/personalProjects/index.js
@@ -9,10 +9,7 @@ export const PersonalProjects = () => {
         <Fragment>
             <div className="container-fluid mt-5 personalProjectsBox">
                 <h2 className="display-3 text-center pt-2">Projects</h2>
-                <div>
-                    <p className="text-center mt-3">Nav-Projects</p>
-                    <p className="text-center mt-3 pb-3">Grid-Projects</p>
-                </div>
+                
             </div>
         </Fragment>
     )
