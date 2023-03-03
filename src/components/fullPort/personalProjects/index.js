347da33842ddcfc729549e03324a1ed1b8a15274
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2023-03-03 01:48:06 -0300

    Commit Att

--- src/components/fullPort/personalProjects/index.js
+++ src/components/fullPort/personalProjects/index.js
@@ -1,6 +1,7 @@
 import {React, Fragment} from 'react'
 import {GridProjects} from '../../../shared/gridProjects'
 import {ItemProject} from '../../../shared/gridProjects/itemProject'
+import {Link} from 'react-router-dom'
 
 import './personalProjects.css'
 
@@ -13,8 +14,13 @@ export const PersonalProjects = () => {
             <div className="container-fluid mt-5 personalProjectsBox">
                 <h2 className="display-3 text-center pt-2 pb-3">Projects</h2>
                 <GridProjects>
-                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogioanalogico.jpeg" textAlt="Relogio Analogico"/>
-                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogiodigital.jpeg" textAlt="Relogio Digital"/>
+                        <Link to="/portifolio/clockanalogic">
+                            <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogioanalogico.jpeg" textAlt="Relogio Analogico"/>
+                        </Link>
+                        <Link to="/portifolio/clockdigital">
+                            <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogiodigital.jpeg" textAlt="Relogio Digital"/>
+                        </Link>
+                        
                         <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/jogodavelha.jpeg" textAlt="Jogo da Velha"/>
                         <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/calculadora.jpeg" textAlt="Calculadora"/>
                         <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/public/coming-soon.png" textAlt=""/>
--- src/components/fullPort/portHeader/index.js
+++ src/components/fullPort/portHeader/index.js
@@ -11,7 +11,7 @@ export const PortHeader = () => {
     return (
         <Fragment>
             <h1 className="display-1 text-center mt-5 portTitle">Portifólio</h1>
-            <div className="container-fluid">
+            <div className="container-fluid portText">
                 <ul className="portHeader-list mt-5">
                     <PortHeaderItem titleItem="Responsividade" description="Responsivo para os diversos tamanhos de dispositivos" srcLink='https://img.icons8.com/ios/50/null/media-queries.png' textAlt="Imagem ilustrativa de responsividade"/>
                     <PortHeaderItem titleItem="Intuitivo" description="Foco na experiencia de forma intuitiva" srcLink="https://img.icons8.com/external-xnimrodx-lineal-xnimrodx/64/null/external-head-design-thinking-xnimrodx-lineal-xnimrodx-3.png" atlText="Imagem ilustrativa de intuitivo"/>
