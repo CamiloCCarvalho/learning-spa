5d4b8b245b27ae02a2cdd274af213abbd5e4a02f
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2023-03-03 00:26:17 -0300

    Commit Att

--- src/components/fullPort/personalProjects/index.js
+++ src/components/fullPort/personalProjects/index.js
@@ -13,10 +13,10 @@ export const PersonalProjects = () => {
             <div className="container-fluid mt-5 personalProjectsBox">
                 <h2 className="display-3 text-center pt-2 pb-3">Projects</h2>
                 <GridProjects>
-                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogioanalogico.jpeg" textAlt=""/>
-                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/public/coming-soon.png" textAlt=""/>
-                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/public/coming-soon.png" textAlt=""/>
-                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/public/coming-soon.png" textAlt=""/>
+                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogioanalogico.jpeg" textAlt="Relogio Analogico"/>
+                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/relogiodigital.jpeg" textAlt="Relogio Digital"/>
+                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/jogodavelha.jpeg" textAlt="Jogo da Velha"/>
+                        <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/src/img/calculadora.jpeg" textAlt="Calculadora"/>
                         <ItemProject linkURL="https://raw.githubusercontent.com/CamiloCCarvalho/my-landing-page/master/public/coming-soon.png" textAlt=""/>
                 </GridProjects>
             </div>
