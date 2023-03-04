8bdf0c5ba4bb347ce9e27bf27de81c8a0ff56620
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2023-03-04 13:46:26 -0300

    Commit Att

--- src/components/fullPort/portHeader/index.js
+++ src/components/fullPort/portHeader/index.js
@@ -13,7 +13,7 @@ export const PortHeader = () => {
             <h1 className="display-1 text-center mt-5 portTitle">Portifólio</h1>
             <div className="container-fluid portText">
                 <ul className="portHeader-list mt-5">
-                    <PortHeaderItem titleItem="Responsividade" description="Responsivo para os diversos tamanhos de dispositivos" srcLink='https://img.icons8.com/ios/50/null/media-queries.png' textAlt="Imagem ilustrativa de responsividade"/>
+                    <PortHeaderItem titleItem="Responsividade" description="Responsivo para os diversos tamanhos de dispositivos" srcLink="https://img.icons8.com/ios-glyphs/64/000000/media-queries.png" textAlt="Imagem ilustrativa de responsividade"/>
                     <PortHeaderItem titleItem="Intuitivo" description="Foco na experiencia de forma intuitiva" srcLink="https://img.icons8.com/external-xnimrodx-lineal-xnimrodx/64/null/external-head-design-thinking-xnimrodx-lineal-xnimrodx-3.png" atlText="Imagem ilustrativa de intuitivo"/>
                 </ul>
             </div>
