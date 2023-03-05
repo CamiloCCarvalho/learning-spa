3419b55eb757630f654ce3f426c6ba12ee30029f
Camilo Costa de Carvalho <asfaltorodas@gmail.com>
2023-03-04 23:12:35 -0300

    Commit Att

--- src/components/projects/hashgame/index.js
+++ src/components/projects/hashgame/index.js
@@ -1,15 +1,109 @@
 import {React, Fragment} from 'react'
 import './hashgame.css'
 
+
 export const HashGame = () => {
+    let isCircleTurn
+    let victoryCombination = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,4,8],
+        [2,4,6],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8]
+    ]
+    const startGame = () => {
+        isCircleTurn = false;
+
+        document.querySelector("[data-rs]").addEventListener('click', startGame)
+        document.querySelector("[data-wm]").classList.remove("show-message")
+        
+        for(const cell of document.querySelectorAll("[data-cell]")) {
+            cell.removeEventListener('click', handleClick)
+            cell.addEventListener('click', handleClick, {once: true})
+            cell.classList.remove('x')
+            cell.classList.remove('circle')
+        }
+        
+        setBoardHoverClass()
+        
+    }
+    const endGame = (isDraw) => {
+        if(isDraw) {
+            document.querySelector("#winMsg").innerText = "Empate!"
+        } else {
+            document.querySelector("#winMsg").innerText = isCircleTurn
+             ? 'O venceu!' 
+             : 'X venceu!'
+        }
+        document.querySelector("[data-wm]").classList.add('show-message')
+    }
+  
+    const placeMark = (cell, classToAdd) => {
+        cell.classList.add(classToAdd)
+    }
+    const checkForWin = (currentPlayer) => {
+        return victoryCombination.some(combination => {
+            return combination.every(index => {
+                return document.querySelectorAll("[data-cell]")[index].classList.contains(currentPlayer)
+            })
+        })
+    }
+    const checkForDraw = (allCells) => {
+        let countMarks = 0
+        for(const cell of allCells){
+            if(cell.classList.contains('x') || cell.classList.contains('circle')){
+                countMarks++
+            }
+        }
+        if(countMarks === 9){
+            return true
+        } else {
+            return false
+        }
+    }
+    const setBoardHoverClass = () => {
+        document.querySelector("[data-bd]").classList.remove("circle")
+        document.querySelector("[data-bd]").classList.remove("x")
+        isCircleTurn 
+            ? document.querySelector("[data-bd]").classList.add("circle")
+            : document.querySelector("[data-bd]").classList.add("x") 
+    }
+    const swapTurns = () => {
+        isCircleTurn = !isCircleTurn
+        setBoardHoverClass()
+    }
+    const handleClick = (e) => {
+        //Input mark
+        const cell = e.target
+        const classToAdd = isCircleTurn ? 'circle' : 'x'
+        placeMark(cell, classToAdd)
+
+        //Check victory
+        const isWin = checkForWin(classToAdd)
+        if(isWin) {
+            endGame(false)
+            console.log("Winner")
+        }
+        //Check a draw
+        const isDraw = checkForDraw(document.querySelectorAll("[data-cell]"))
+        if(isDraw) {
+            endGame(true)
+        } 
+        //Change mark
+        swapTurns()
+        console.log('clicou na celula')
+    }
 
     return (
         <Fragment>
             <div className="container-fluid hashGameBox">
 
-                <div className="board circle">
-                    <div className="cell x"data-cell></div>
-                    <div className="cell circle"data-cell></div>
+                <div className="board" id="bd" data-bd>
+                    <div className="cell"data-cell></div>
+                    <div className="cell"data-cell></div>
                     <div className="cell"data-cell></div>
 
                     <div className="cell"data-cell></div>
@@ -22,10 +116,13 @@ export const HashGame = () => {
                 </div>
 
                 <div className="winning-message"data-wm>
-                    <p className="winnig-message-text">X foi o vencedor!</p>
-                    <button className="winning-message-btn">Reiniciar</button>
+                    <p className="winnig-message-text" id="winMsg">X foi o vencedor!</p>
+                    <button className="winning-message-btn"data-rs>Reiniciar !</button>
                 </div>
             </div>
+            <script>
+                {setTimeout(startGame, 1000)}
+            </script>
         </Fragment>
     )
 }
